3a7229bad5d78cd168e4d2d64690ddff0c124560
JORDAN JONES <jjones42@wisc.edu>
2019-08-19 16:03:58 -0500

    Update README and fix errors about const

--- src/app/element/element.component.ts
+++ src/app/element/element.component.ts
@@ -13,14 +13,20 @@ import {
   encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ElementComponent {
+  MAINTENANCE_LIFECYCLE = "MAINTENANCE"
   @Input() title: string; // Card title
   @Input() description: string; // Card description
   @Input() content: string;
   @Input() fname: string; // Unique name of the card frame
+  @Input() lifecycle: string;
 
   @Output() deleteCardNotify = new EventEmitter<boolean>();
 
   handleCardDelete() {
     this.deleteCardNotify.emit(true);
+  }
+
+  isMaintenanceMode() {
+    return (this.lifecycle === this.MAINTENANCE_LIFECYCLE);
   }
 }
